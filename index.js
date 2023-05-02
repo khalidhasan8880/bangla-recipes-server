f749bc5ff81719803e13a350eeb0bf353f810a08
khalidhasan8880 <khalidhasan8880@gmail.com>
2023-05-02 23:05:14 +0600

    changed data

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ const port = process.env.PORT || 5000
 const cors = require('cors')
 app.use(cors())
 
-const data = require('./data/recipes.json')
+const data = require('./data/chef.json')
 app.get('/', (req, res) => {
   res.send(data)
 })
-app.get('/recipe/:id', (req, res) => {
-  const id  = req.params.id;
-  const item = data.recipes.find(recipe=> recipe.id == id)
-  res.send(item)
-})
+// app.get('/recipe/:id', (req, res) => {
+//   const id  = req.params.id;
+//   const item = data.recipes.find(recipe=> recipe.id == id)
+//   res.send(item)
+// })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)

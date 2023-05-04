04f01196063863b4bdfb04a980ffa9d5d109e851
khalidhasan8880 <khalidhasan8880@gmail.com>
2023-05-04 08:06:14 +0600

    added recipe id for each recipe

--- index.js
+++ index.js
@@ -9,9 +9,14 @@ app.get('/', (req, res) => {
   res.send(data)
 })
 app.get('/chef/:id', (req, res) => {
-  const id  = req.params.id;
-  const chef = data.chefs.find(chef=> chef.id == id)
+  const id = req.params.id;
+  const chef = data.chefs.find(chef => chef.id == id)
   res.send(chef)
+})
+app.get('/recipe/:id', (req, res) => {
+  const id = req.params.id;
+  const recipe = data.chefs.find(chef => chef.recipes.find(rs=> rs.recipeId == id))
+  res.send(recipe)
 })
 
 app.listen(port, () => {
